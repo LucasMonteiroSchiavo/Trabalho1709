38c8ef6bc00fc1180c6e740620c38e0c337ee696
mxwnie <177383266+mxwnie@users.noreply.github.com>
2025-09-18 20:06:22 -0300

    interface html conectada a api

--- src/main/java/Trabalho/demo/controller/FuncionarioController.java
+++ src/main/java/Trabalho/demo/controller/FuncionarioController.java
@@ -10,6 +10,11 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/funcionarios")
+@CrossOrigin(
+        origins = {"http://127.0.0.1:5500", "http://localhost:5500"},
+        allowedHeaders = {"*"},
+        methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS}
+)
 public class FuncionarioController {
 
     @Autowired
